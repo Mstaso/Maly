13f49475d70aef4eb60be1cd53d7e49c174aefe4
MaleehaBhuiyan <maleehabhuiyan@baruchmail.cuny.edu>
2020-08-12 15:37:35 -0400

    post, postcontainer made

---
+++ malyfrontend/src/Components/Post.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Post(){
+    return(
+        <div>
+            Hi from post
+        </div>
+    )
+}
+
+export default Post
---
+++ malyfrontend/src/Containers/PostContainer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Post from '../Components/Post'
+
+function PostContainer(){
+    return(
+        <div>
+            Hello from post container
+            <Post />
+        </div>
+    )
+}
+
+export default PostContainer
